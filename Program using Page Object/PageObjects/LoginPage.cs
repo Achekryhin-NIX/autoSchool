c8979e8d00d022c2d0040eb341a8cedc7a6e82cf
Achekryhin-NIX <93332933+Achekryhin-NIX@users.noreply.github.com>
2021-11-19 11:33:45 +0200

    Add files via upload

---
+++ Program using Page Object/PageObjects/LoginPage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumExtras.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium.Remote;
+
+namespace SELENIUM_New.PageObjects
+{
+    class LoginPage
+    {
+        [FindsBy(How = How.CssSelector, Using = "#email")]
+        public IWebElement emailElement;
+
+        [FindsBy(How = How.Id, Using = "passwd")]
+        public IWebElement passwordElement;
+
+        [FindsBy(How = How.Id, Using = "SubmitLogin")]
+        public IWebElement loginElement;
+
+        // task 2.1
+        [FindsBy(How = How.CssSelector, Using = "#create-account_form > div > p")]
+        public IWebElement CreateAccountForm;
+        // task 2.2
+        [FindsBy(How = How.CssSelector, Using = "#login_form > h3")]
+        public IWebElement LoginForm;
+        // // task 2.3
+        [FindsBy(How = How.CssSelector, Using = "#SubmitCreate > span:nth-child(1)")]
+        public IWebElement SubmitCreate;
+
+
+
+
+    }
+}
---
+++ Program using Page Object/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumExtras.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium.Remote;
+using SELENIUM_New.PageObjects;
+
+namespace SELENIUM_New
+{
+    class Program
+    {
+        // 2.1
+        static void Main()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php?controller=authentication&back=my-account");
+            LoginPage loginPage = new LoginPage();
+            PageFactory.InitElements(driver, loginPage);
+            String text = loginPage.CreateAccountForm.Text;
+            Console.WriteLine(text);
+            Console.Read();
+        }
+
+        // 2.2
+        static void main()
+
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php?controller=authentication&back=my-account");
+            LoginPage loginPage = new LoginPage();
+            PageFactory.InitElements(driver, loginPage);
+            String text = loginPage.LoginForm.Text;
+            Console.WriteLine(text);
+            Console.Read();
+        }
+        // Task 2.3
+
+        static void Maiin()
+
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php?controller=authentication&back=my-account");
+            LoginPage loginPage = new LoginPage();
+            PageFactory.InitElements(driver, loginPage);
+            String text = loginPage.SubmitCreate.Text;
+            Console.WriteLine(text);
+            Console.Read();
+        }
+
+        // TASK 3
+
+        class MainTest
+        {
+            static void Main()
+            {
+                var driver = new ChromeDriver();
+                driver.Navigate().GoToUrl("http://automationpractice.com/index.php?controller=authentication&back=my-account");
+
+                LoginPage loginPage = new LoginPage();
+                PageFactory.InitElements(driver, loginPage);
+
+                loginPage.emailElement.SendKeys("Peter@UA");
+                Thread.Sleep(500);
+
+                loginPage.passwordElement.SendKeys("12345");
+                Thread.Sleep(500);
+
+                loginPage.loginElement.Click();
+
+            }
+
+
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+        
+    
+
+
