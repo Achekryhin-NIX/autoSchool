7bd6ddc9fbf187f6f5ae6e5f29fa713936b8da51
Achekryhin-NIX <93332933+Achekryhin-NIX@users.noreply.github.com>
2022-01-23 23:20:08 +0200

    Add files via upload

---
+++ TestTas/TestTaskA/Program.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium.Chrome;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Threading;
+using TestTaskA.Selenium_Pages;
+
+namespace TestTaskA
+{    /*Task 1*/
+    //1. Open WebPage "https://duckduckgo.com/".
+    //2. Type any word in the search field.
+    //3. Click "Search" button.
+    //4. Check that a certain amount of search results are displayed on the page, remember the quantity of search results.
+    //5. Scroll down and click "More Resulst" button.
+    //6. Check that more test results are displayed.
+    class Program
+    {
+    static void Main()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://duckduckgo.com/");
+            
+            MainPage mainPage = new MainPage();
+            PageFactory.InitElements(driver, mainPage);
+            mainPage.SearchFieldInput.SendKeys("Summer");
+            
+            mainPage.SearchButton.Click();
+            Thread.Sleep(500);
+
+            var resultPage = new ResultPage();
+            resultPage.MoreResultButton.Click();
+        }
+    }
+}
+
+//Task 2
+
+//    По автоматизации API задание пока затрудняюсь выполнить, так как в автоматизации тесты писал
+//    только по UI. (На данный момент API тестировал только мануально).
+
+//    ПС. В моём профиле ГИТа, при желании, можете посмотреть другие мои работы (тесты с использованием 
+//    Spec Flow, Page Objects, применения Linq, OOP и т.д.).
+    
+//    Благодарю за внимание.
---
+++ TestTas/TestTaskA/Selenium Pages/MainPage.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestTaskA.Selenium_Pages
+{
+    class MainPage
+    {
+        [FindsBy(How = How.CssSelector, Using = "#search_form_input_homepage")]
+        public IWebElement SearchFieldInput;
+
+        [FindsBy(How = How.CssSelector, Using = "#search_button_homepage")]
+        public IWebElement SearchButton;
+    }
+}
